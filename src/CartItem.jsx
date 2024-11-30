a2088b31e40a73331058293ea52dafdf67e3d7e0
Samo-cat <samosational@gmail.com>
2024-11-30 15:41:12 -0500

    initial commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,7 +9,7 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
-        const totalAmount = cart = item.cost * item.quantity, 0 ;
+        const totalCartAmount = item.cost * item.quantity, 0 ;
   };
 
   const handleContinueShopping = (e) => {
