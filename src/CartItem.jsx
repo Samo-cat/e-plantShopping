9428f9dcd396a6609ac0d5980068790a54ed2dc5
Samo-cat <samosational@gmail.com>
2024-11-30 14:45:39 -0500

    initial commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,28 +9,57 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+        const totalAmount = cart = item.cost * item.quantity, 0 ;
   };
 
   const handleContinueShopping = (e) => {
-   
+    const onContinueShopping = useSelector(state => state.productList.plantsArray.);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    increaseItemQuantity(state, action) {
+        const itemToIncrease = state.cartItems.find(item => item.id === action.payload);
+        if (itemToIncrease) {
+          itemToIncrease.quantity += 1;
+        }
+      }
+        dispatch(updateQuantity(item));
+    }
   };
 
   const handleDecrement = (item) => {
-   
+    decreaseItemQuantity(state, action) {
+        const itemToDecrease = state.cartItems.find(item => item.id === action.payload);
+        if (itemToDecrease && itemToDecrease.quantity > 1) {
+          itemToDecrease.quantity -= 1;
+        }
+      }
+        dispatch(updateQuantity(item))
+    }
   };
 
   const handleRemove = (item) => {
+    removeItemFromCart(state, action) {
+        state.cartItems = state.cartItems.filter(item => item.id !== action.payload);
+      },
+      clearCart(state) {
+        state.cartItems = [];
+      },
+    dispatch(removeItem(item))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let Total = 0;
+    plantsArray.forEach(item) = {
+        total = item.cost * item.quantity;
+    }
   };
+
 
   return (
     <div className="cart-container">
